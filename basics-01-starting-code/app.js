76df31719a1eb1c5f9824e65c411278d1652bc8e
Leehuseung <gntmd4261@naver.com>
2023-05-28 10:49:53 +0900

    2-17. method 이해하기

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -4,6 +4,16 @@ const app = Vue.createApp({
             courseGoal: 'Finish the course are learn Vue!',
             vueLink: 'https://vuejs.org/' // v-bind 를 이용해  html "" 안에 변수를 사용할 수 있다
         }
+    },
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5){
+                return 'Learn Vue!';
+            } else {
+                return 'Master Vue!'
+            }
+        }
     }
 });
 
