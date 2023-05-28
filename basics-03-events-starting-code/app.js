7f8e2c5ffbf14e520a07365396c5817703da97b4
Leehuseung <gntmd4261@naver.com>
2023-05-28 11:57:30 +0900

    2-25. 이벤트 수식어 사용

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -3,10 +3,18 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      confirmedName: ''
     };
   },
   methods: {
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm(event){
+      // event.preventDefault();
+      // alert('submit');
+    },
     add(num) {
       this.counter += num;
     },
