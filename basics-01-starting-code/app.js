65b3439a5827d06f6c2236de6c5f7ba412245e4b
Leehuseung <gntmd4261@naver.com>
2023-05-28 10:58:00 +0900

    2-19. v-html 사용

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoal: 'Finish the course are learn Vue!',
+            courseGoalA: 'Finish the course are learn Vue!',
+            courseGoalB: '<h2>Master Vue and build</h2>',
             vueLink: 'https://vuejs.org/' // v-bind 를 이용해  html "" 안에 변수를 사용할 수 있다
         }
     },
@@ -9,9 +10,9 @@ const app = Vue.createApp({
         outputGoal() {
             const randomNumber = Math.random();
             if (randomNumber < 0.5){
-                return 'Learn Vue!';
+                return this.courseGoalA;
             } else {
-                return 'Master Vue!'
+                return this.courseGoalB;
             }
         }
     }
