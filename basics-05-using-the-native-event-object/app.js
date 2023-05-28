db9d394167f9581d11af636c95f8576eacf0aa49
Leehuseung <gntmd4261@naver.com>
2023-05-28 12:58:24 +0900

    2-29. 연산 프로퍼티

--- basics-05-using-the-native-event-object/app.js
+++ basics-05-using-the-native-event-object/app.js
@@ -5,8 +5,28 @@ const app = Vue.createApp({
       name: ''
     };
   },
+  computed: {
+    //함수처럼 사용하지 않고 변수처럼 사용한다.
+    fullname(){
+      //computed 프로퍼티를 사용하면 counter에대한 이벤트 조작 사용시 재랜더링 하지 않는다.
+      //name 이 변경된 경우에만 다시 계산한다.
+      console.log('Running again...');
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + 'abcdef';
+    }
+  },
   methods: {
-    setName(event, lastName) {
+    outputFullname() {
+      //counter를 up down 해도 실행된다. 더 나은 해결책은??
+      console.log('Running again');
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + 'abcdef';
+    },
+    setName(event) {
       this.name = event.target.value;
     },
     add(num) {
