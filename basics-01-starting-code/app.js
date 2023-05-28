d803a526e7403118127cb1953d761aca6abda5ab
Leehuseung <gntmd4261@naver.com>
2023-05-28 10:42:22 +0900

    2-16. v-bind

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoal: 'Finish the course are learn Vue!'
+            courseGoal: 'Finish the course are learn Vue!',
+            vueLink: 'https://vuejs.org/'
         }
     }
 });
