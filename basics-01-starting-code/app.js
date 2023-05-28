48890baad441e477fc7cc35017bca67b27faa78e
Leehuseung <gntmd4261@naver.com>
2023-05-28 10:42:54 +0900

    2-16. v-bind 주석 추가

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             courseGoal: 'Finish the course are learn Vue!',
-            vueLink: 'https://vuejs.org/'
+            vueLink: 'https://vuejs.org/' // v-bind 를 이용해  html "" 안에 변수를 사용할 수 있다
         }
     }
 });
