19a95a8e3319f83ed2f6d9e20a2cc60b98d0ebde
Leehuseung <gntmd4261@naver.com>
2023-05-30 21:30:50 +0900

    6-71. 컴포넌트 변경

--- cmp-intro-01-starting-setup/app.js
+++ cmp-intro-01-starting-setup/app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
     data() {
         return {
-          detailsAreVisible: false,
           friends: [
               {
                   id: 'manuel',
@@ -16,6 +15,34 @@ const app = Vue.createApp({
                   email: 'julie@localhost.com'
               },
           ],
+        };
+    },
+});
+
+// app에게 컴포넌트를 만들겠다고 알려줌.
+// 첫번째 인수 식별자 대시를 포함한 항상 두단어로 해야한다. html 충돌 되기 때문에..
+// 두번 째 인수 createApp과 같은 구성객체
+// 앱에 하위 앱
+app.component('friend-contact', {
+    template: `
+            <li>
+                <h2>{{ friend.name}}</h2>
+                <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show'}} Details</button>
+                <ul v-if="detailsAreVisible">
+                    <li><strong>Phone:</strong> {{friend.phone}}</li>
+                    <li><strong>Email:</strong> {{friend.email}}</li>
+                </ul>
+            </li>
+    `,
+    data() {
+        return {
+            detailsAreVisible:false,
+            friend: {
+                id: 'manuel',
+                name: 'Manuel Lorenz',
+                phone: '01234 5678 991',
+                email: 'manuel@localhost.com'
+            },
         };
     },
     methods: {
