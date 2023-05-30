0d228187658f83d5f46053e06e3f439e6fd47213
Leehuseung <gntmd4261@naver.com>
2023-05-30 21:58:58 +0900

    15-213. 저장소 생성

--- vuex-01-starting-setup/src/App.vue
+++ vuex-01-starting-setup/src/App.vue
@@ -1,14 +1,12 @@
 <template>
-  <base-container title="Vuex"></base-container>
+  <base-container title="Vuex">
+    <h3>{{ $store.state.counter }}</h3>
+    <button>Add 1</button>
+  </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
-
-console.log(1);
-console.log(1);
-console.log(1);
-console.log(1);
 
 export default {
   components: {
