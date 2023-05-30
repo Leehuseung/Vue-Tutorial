4ade1e3d6fb7d8318cf86ade5736470494aba696
Leehuseung <gntmd4261@naver.com>
2023-05-30 22:49:02 +0900

    15-217. MapperHelper 적용

--- vuex-01-starting-setup/src/components/ChangeCounter.vue
+++ vuex-01-starting-setup/src/components/ChangeCounter.vue
@@ -1,17 +1,28 @@
 <template>
-  <button @click="addOne">Add 1</button>
+  <button @click="increment">Add 2</button>
+  <button @click="increase({value:11})">Add 11</button>
 </template>
 
 <script>
+import {mapActions} from "vuex";
 export  default  {
   methods: {
-    addOne() {
-      //store에 mutation에서 정의한함수.
-      // this.$store.commit('increment');
+    // addOne() {
+    //   //store에 mutation에서 정의한함수.
+    //   // this.$store.commit('increment');
+    //
+    //   //actions 실행
+    //   this.$store.dispatch('increment');
+    // }
 
-      //actions 실행
-      this.$store.dispatch('increment');
-    }
+    //actions를 한번에 선언한다. 위에처럼 addOne이 필요 없다.
+    ...mapActions(['increment', 'increase'])
+
+    // ...mapActions({
+    //     inc: 'increment',  //매핑되어야하는 키를 넣어준다.
+    //     increase: 'increase' //
+    //   })
+    //<button @click="inc">Add 2</button>  이렇게 사용한다.
   }
 }
 </script>
--- vuex-01-starting-setup/src/components/TheCounter.vue
+++ vuex-01-starting-setup/src/components/TheCounter.vue
@@ -1,13 +1,18 @@
 <template>
-  <h3>{{ counter }}</h3>
+<!--  <h3>{{ counter }}</h3>-->
+  <h3>{{ finalCounter }}</h3>
 </template>
 
 <script>
+import {mapGetters} from "vuex";
 export default {
   computed: {
-    counter() {
-      return this.$store.getters.finalCounter;
-    }
+    // counter() {
+    //   return this.$store.getters.finalCounter;
+    // }
+    //연산(computed) 프로퍼티에 병합되는 객체를 제공한다. 위에처럼 선언을 안해도 된다.
+    //게터 여러개 사용시 코드가 효율적임.
+    ...mapGetters(['finalCounter'])
   },
 }
 </script>
