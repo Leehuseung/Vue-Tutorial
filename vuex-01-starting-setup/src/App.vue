29ad92b16b990c4f3ecc6a05a2cf3b5198151165
Leehuseung <gntmd4261@naver.com>
2023-05-31 02:12:27 +0900

    15-221. vuex를 이용한 로그인 상태 구현

--- vuex-01-starting-setup/src/App.vue
+++ vuex-01-starting-setup/src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <base-container title="Vuex">
+  <base-container title="Vuex" v-if="isAuth">
 <!--    <h3>{{ $store.state.counter }}</h3>-->
 <!--    <h3>{{ counter }}</h3>-->
     <the-counter></the-counter>
     <favorite-value></favorite-value>
     <button @click="addOne">Add 1</button>
     <change-counter></change-counter>
+  </base-container>
+  <base-container title="Auth">
+    <user-auth>
+
+    </user-auth>
   </base-container>
 </template>
 
@@ -14,9 +19,11 @@ import BaseContainer from './components/BaseContainer.vue';
 import TheCounter from "@/components/TheCounter.vue";
 import ChangeCounter from "@/components/ChangeCounter.vue";
 import FavoriteValue from "@/components/FavoriteValue.vue";
+import UserAuth from "@/components/UserAuth.vue";
 
 export default {
   components: {
+    UserAuth,
     FavoriteValue,
     ChangeCounter,
     BaseContainer,
@@ -25,6 +32,9 @@ export default {
   computed: {
     counter() {
       return this.$store.state.counter;
+    },
+    isAuth() {
+      return this.$store.getters.userIsAuthenticated;
     }
   },
   methods: {
