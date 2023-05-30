cd8d3a3108ed1e4ded1f6db10939b05d70f59a72
Leehuseung <gntmd4261@naver.com>
2023-05-30 21:36:07 +0900

    15. vuex 세팅

---
+++ vuex-01-starting-setup/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <base-container title="Vuex"></base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+
+console.log(1);
+console.log(1);
+console.log(1);
+console.log(1);
+
+export default {
+  components: {
+    BaseContainer,
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
