6543ef5a733e690b8f670b27cc33f42b9bab2d17
Leehuseung <gntmd4261@naver.com>
2023-05-30 22:28:32 +0900

    15-217. Getter 소개

--- vuex-01-starting-setup/src/App.vue
+++ vuex-01-starting-setup/src/App.vue
@@ -3,19 +3,21 @@
 <!--    <h3>{{ $store.state.counter }}</h3>-->
 <!--    <h3>{{ counter }}</h3>-->
     <the-counter></the-counter>
+    <favorite-value></favorite-value>
     <button @click="addOne">Add 1</button>
     <change-counter></change-counter>
   </base-container>
-
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
 import TheCounter from "@/components/TheCounter.vue";
 import ChangeCounter from "@/components/ChangeCounter.vue";
+import FavoriteValue from "@/components/FavoriteValue.vue";
 
 export default {
   components: {
+    FavoriteValue,
     ChangeCounter,
     BaseContainer,
     TheCounter,
--- vuex-01-starting-setup/src/main.js
+++ vuex-01-starting-setup/src/main.js
@@ -13,13 +13,26 @@ const store = createStore({
         //여러 컴포넌트에서 counter를 접근하는게 아니라 이 함수를 이용해 바꾼다.
         //중복제거, 실수제거 등 가능.
         increment(state) {
-            state.counter += 1;
+            state.counter += 2;
         },
         //payload는 객체, 숫자 ,, 종류는 상관없다.
         increase(state, payload) {
             state.counter = state.counter + payload.value;
         }
-    }
+    },
+    getters: {
+        //여기서만 바꾸면 된다.
+        finalCounter(state) {
+            return state.counter * 2;
+        },
+        nomalizedCounter(_, getters) {
+            //getters를 이용해 finalCounter (중복되지않도록) 계산된 값을 가져온다
+            const finalCounter = getters.finalCounter;
+            if(finalCounter < 0) return 0;
+            if(finalCounter > 100)return 100;
+            return finalCounter;
+        }
+     }
 });
 
 const app = createApp(App);
