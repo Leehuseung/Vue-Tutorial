e139895fcd1522b137150b9d8b2168a67c780548
Leehuseung <gntmd4261@naver.com>
2023-06-05 17:53:02 +0900

    9-111. 전역 컴포넌트 -> 지역 컴포넌트 변경

--- cmp-adv-01-starting-setup/src/main.js
+++ cmp-adv-01-starting-setup/src/main.js
@@ -1,16 +1,20 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
-import TheHeader from './components/TheHeader.vue';
+// import TheHeader from './components/TheHeader.vue';
 import BaseBadge from './components/BaseBadge.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
+// import BadgeList from './components/BadgeList.vue';
+// import UserInfo from './components/UserInfo.vue';
 
 const app = createApp(App);
-
-app.component('the-header', TheHeader);
+/**
+ * 전역 컴포넌트로 등록되게 된다.
+ *
+ * 전역 등록시 애플리케이션 로드할 때 vue 컴포넌트 전부를 로드해야한다.
+ */
+// app.component('the-header', TheHeader);
 app.component('base-badge', BaseBadge);
-app.component('badge-list', BadgeList);
-app.component('user-info', UserInfo);
+// app.component('badge-list', BadgeList);
+// app.component('user-info', UserInfo);
 
 app.mount('#app');
