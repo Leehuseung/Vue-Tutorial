266d6807bb8dc971c60cf0e22760a7af4416a5f1
Leehuseung <gntmd4261@naver.com>
2023-06-05 22:24:09 +0900

    9-121. teleport를 활용한 모달 컴포넌트의 생성 위치 변경

--- cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -3,11 +3,14 @@
   <h2>Manage Goals</h2>
   <input type="text" ref="goal"/>
   <button @click="setGoal">Set Goal</button>
-  <error-alert v-if="inputIsInvalid">
-    <h2>Input is invalid!</h2>
-    <p>Please enter at leasy a few character...</p>
-    <button @click="confirmError">Okay</button>
-  </error-alert>
+<!--  teleport 기능은 error-alert 같은 모달의 위치를 컴포넌트 안이 아니라 원하는 위치로 이동시킬 수 있다.-->
+  <teleport to="body">
+    <error-alert v-if="inputIsInvalid">
+      <h2>Input is invalid!</h2>
+      <p>Please enter at leasy a few character...</p>
+      <button @click="confirmError">Okay</button>
+    </error-alert>
+  </teleport>
 </div>
 </template>
 
