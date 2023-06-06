cd4ac8520bbd526b42b8963c4db1168d65e9073e
Leehuseung <gntmd4261@naver.com>
2023-06-06 15:14:36 +0900

    11-144. 유효성 검사 구현

--- forms-01-starting-setup/src/components/TheForm.vue
+++ forms-01-starting-setup/src/components/TheForm.vue
@@ -1,8 +1,9 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control">
+    <div class="form-control" :class="{invalid: userNameValidity === 'invalid'}">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" v-model="userName"/>
+      <input id="user-name" name="user-name" type="text" v-model.trim="userName" @blur="validateInput"/>
+      <p v-if="userNameValidity === 'invalid'">Please enter a valid</p>
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
@@ -70,6 +71,7 @@ export default {
       interest: [], //기본값을 null 로 할경우에 한개만 선택해도 모두 선택됨.
       how: null,
       confirm: false,
+      userNameValidity: 'pending',
     };
   },
   methods: {
@@ -95,6 +97,13 @@ export default {
       console.log('confirm')
       console.log(this.confirm);
       this.confirm=false;
+    },
+    validateInput() {
+      if (this.userName === '') {
+        this.userNameValidity = 'invalid';
+      } else {
+        this.userNameValidity = 'valid';
+      }
     }
   }
 }
@@ -112,6 +121,14 @@ form {
 
 .form-control {
   margin: 0.5rem 0;
+}
+
+.form-control.invalid input {
+  border-color: red;
+}
+
+.form-control.invalid label {
+  color:red;
 }
 
 label {
@@ -162,4 +179,6 @@ button:active {
   border-color: #002350;
   background-color: #002350;
 }
+
+
 </style>
